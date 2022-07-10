6e989bb60883f9be9374c13caace97cbc3905291
Kristian Brox <kristian.brox@pm.me>
2022-07-10 11:18:06 +0200

    Adding some changes to test map and jump hight

--- godot/src/Actors/Actor.gd
+++ godot/src/Actors/Actor.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 class_name Actor
 
 const FLOOR_NORMAL := Vector2.UP
-export var speed := Vector2(300.0, 600.0)
+export var speed := Vector2(300.0, 650.0)
 export var gravity := 2000.0
 var _velocity := Vector2.ZERO
 
